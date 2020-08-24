0259d430b37a2f828cf36a556a3d0422fc78711a
Kai Frey <kai.m.frey.22@dartmouth.edu>
2020-08-24 15:46:24 -0400

    system

--- src/components/solar_system.js
+++ src/components/solar_system.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Slider } from 'rsuite';
 import { changeSpeed } from '../actions';
+
+import SystemPlanet from './system_planet';
 // import { NavLink } from 'react-router-dom';
 import { getPlanets, createPlanet } from '../actions/planetApi';
 
 class SolarSystem extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-
+      // AU to pixels
+      sizeFactor = window.innerWidth/70,
     };
   }
 
@@ -23,11 +25,21 @@ class SolarSystem extends Component {
     this.props.createPlanet();
   }
 
+  createSystem = () => {
+    html = "<div className='all-planets'>";
+    for (id = 1; id < 9; id++) {
+      html += `<SystemPlanet sizeFactor=${this.state.sizeFactor} id=${id}/>`
+    }
+    html += "</div>";
+    return html;
+  }
+
   render() {
     return (
       <div className="mainpage">
         Solar System Page!
-        <button type="button" className="default-button nav-button" onClick={this.handleCreatePlanet}>Create new planet</button>
+        <button type="button" className="default-button nav-button" onClick={this.handleCreatePlanet}>Create new planet</button> {/* Button to create arbitrary planet */}
+        { this.createSystem() }
         <div className='speed-slider'>
           <div>Orbit Speed</div>
           <Slider defaultValue={100}
